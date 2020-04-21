d061504430bfa0b49f42b7a746c57586ef439efe
Dong_Hyck_Na <43693001+agvx124@users.noreply.github.com>
2020-04-21 23:54:22 +0900

    [FIX] MainActivity
    Base 적용

--- app/src/main/java/com/na/dgsw/gongyou_android/ui/main/MainActivity.kt
+++ app/src/main/java/com/na/dgsw/gongyou_android/ui/main/MainActivity.kt
@@ -3,16 +3,34 @@ package com.na.dgsw.gongyou_android.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.na.dgsw.gongyou_android.R
+import com.na.dgsw.gongyou_android.databinding.ActivityMainBinding
+import com.na.dgsw.gongyou_android.ui.base.BaseActivity
 
 /**
  * Created by NA on 2020-04-16
  * skehdgur8591@naver.com
  */
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun getLayoutId(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getViewModel(): MainViewModel {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBindingVariable(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun setUp() {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/na/dgsw/gongyou_android/ui/main/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.na.dgsw.gongyou_android.ui.main
+
+import android.app.Application
+import com.na.dgsw.gongyou_android.ui.base.BaseViewModel
+
+
+/**
+ * Created by NA on 2020-04-21
+ * skehdgur8591@naver.com
+ */
+
+class MainViewModel(application: Application): BaseViewModel<Any>(application) {
+
+}
