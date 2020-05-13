d54c8adbd4519535fc6797344088d93418074cea
Dong_Hyck_Na <43693001+agvx124@users.noreply.github.com>
2020-05-14 00:17:31 +0900

    [FIX] GetTotalInternalMemorySize, GetFileSize
    코드 수정

--- app/src/main/java/com/na/dgsw/gongyou_android/activity/main/MainActivity.kt
+++ app/src/main/java/com/na/dgsw/gongyou_android/activity/main/MainActivity.kt
@@ -11,6 +11,7 @@ import com.na.dgsw.gongyou_android.viewmodel.MainViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 import java.io.File
 import java.lang.StringBuilder
+import java.text.DecimalFormat
 
 /**
  * Created by NA on 2020-04-16
@@ -38,41 +39,65 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     }
 
-    @SuppressWarnings("deprecation")
-    fun getTotalInternalMemorySize(): String {
-        var path: File = Environment.getDataDirectory();
-        var stat: StatFs = StatFs(path.path)
-        var blockSize: Long = stat.blockSizeLong
-        var totalBlocks: Long = stat.blockCountLong
-
-        return formatSize(totalBlocks * blockSize);
+    private fun isExternalMemoryAvailable(): Boolean {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)
     }
 
-    fun formatSize(size: Long): String {
-        var suffix: String? = null
-        var sizeIndex: Long = size;
+    private fun getTotalInternalMemorySize(): String {
 
-        if (sizeIndex >= 1024) {
-            suffix = " KB"
-            sizeIndex /= 1024
-            if (sizeIndex >= 1024) {
-                suffix = " MB"
-                sizeIndex /= 1024
-            }
+        if (isExternalMemoryAvailable()) {
+            var statFs: StatFs = StatFs(Environment.getExternalStorageDirectory().path)
+            var blockSize: Long = statFs.blockSizeLong
+            var totalBlock: Long = statFs.blockCountLong
+
+            return getFileSize(blockSize * totalBlock)
+        }
+        else {
+            return getFileSize(0)
         }
 
-        var resultBuffer: StringBuilder = StringBuilder(sizeIndex.toString())
 
-        var commaOffset: Int = resultBuffer.length - 3
-        while (commaOffset > 0) {
-            resultBuffer.insert(commaOffset, ',')
-            commaOffset -= 3
-        }
+//        var path: File = Environment.getDataDirectory();
+//        var stat: StatFs = StatFs(path.path)
+//        var blockSize: Long = stat.blockSizeLong
+//        var totalBlocks: Long = stat.blockCountLong
 
-        if (suffix != null) resultBuffer.append(suffix)
-
-        return resultBuffer.toString()
+//        return getFileSize(totalBlocks * blockSize);
     }
+
+    private fun getFileSize(size: Long): String {
+        if (size <= 0) return "0"
+
+        val units: Array<String> = arrayOf("B", "KB", "MB", "GB", "TB");
+        var digitGroups: Int = (Math.log10(size.toDouble()) / Math.log10(1024.0)).toInt()
+        return DecimalFormat("#,##0.#").format(size / Math.pow(1024.0, digitGroups.toDouble())) + " " + units[digitGroups]
+    }
+
+//    fun formatSize(size: Long): String {
+//        var suffix: String? = null
+//        var sizeIndex: Long = size;
+//
+//        if (sizeIndex >= 1024) {
+//            suffix = " KB"
+//            sizeIndex /= 1024
+//            if (sizeIndex >= 1024) {
+//                suffix = " MB"
+//                sizeIndex /= 1024
+//            }
+//        }
+//
+//        var resultBuffer: StringBuilder = StringBuilder(sizeIndex.toString())
+//
+//        var commaOffset: Int = resultBuffer.length - 3
+//        while (commaOffset > 0) {
+//            resultBuffer.insert(commaOffset, ',')
+//            commaOffset -= 3
+//        }
+//
+//        if (suffix != null) resultBuffer.append(suffix)
+//
+//        return resultBuffer.toString()
+//    }
 
 }
 
