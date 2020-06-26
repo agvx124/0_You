c86e4ccc320da64edd6404325da7bac4e1bffaa2
Dong_Hyck_Na <43693001+agvx124@users.noreply.github.com>
2020-06-26 11:56:24 +0900

    [ADD] QrCode Scanner Sync

--- app/src/main/java/com/na/dgsw/gongyou_android/viewmodel/GetMainViewModel.kt
+++ app/src/main/java/com/na/dgsw/gongyou_android/viewmodel/GetMainViewModel.kt
@@ -2,6 +2,7 @@ package com.na.dgsw.gongyou_android.viewmodel
 
 import android.app.Application
 import com.na.dgsw.gongyou_android.base.BaseViewModel
+import com.na.dgsw.gongyou_android.utils.SingleLiveEvent
 
 
 /**
@@ -9,4 +10,12 @@ import com.na.dgsw.gongyou_android.base.BaseViewModel
  * skehdgur8591@naver.com
  */
 class GetMainViewModel(application: Application): BaseViewModel<Any>(application) {
+
+    val onQrCodeScannerEvent = SingleLiveEvent<Unit>()
+
+
+    fun qrCodeScannerBtnClick() {
+        onQrCodeScannerEvent.call()
+    }
+
 }
