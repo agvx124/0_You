da01e67b7a7621cb8c2f3f1c74b619eb2f59f00a
Dong_Hyck_Na <43693001+agvx124@users.noreply.github.com>
2020-06-27 22:29:54 +0900

    [ADD] QR Code Scanner on OnClick

--- app/src/main/java/com/na/dgsw/gongyou_android/view/main/GetMainFragment.kt
+++ app/src/main/java/com/na/dgsw/gongyou_android/view/main/GetMainFragment.kt
@@ -1,10 +1,14 @@
 package com.na.dgsw.gongyou_android.view.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import com.blikoon.qrcodescanner.QrCodeActivity
 import com.na.dgsw.gongyou_android.BR
 import com.na.dgsw.gongyou_android.R
 import com.na.dgsw.gongyou_android.base.BaseFragment
@@ -28,6 +32,8 @@ private const val ARG_PARAM2 = "param2"
  */
 class GetMainFragment : BaseFragment<FragmentGetMainBinding, GetMainViewModel>() {
 
+    private val REQUEST_CODE_QR_SCAN = 101
+
     override val viewModelClass: Class<GetMainViewModel>
         get() = GetMainViewModel::class.java
 
@@ -44,7 +50,22 @@ class GetMainFragment : BaseFragment<FragmentGetMainBinding, GetMainViewModel>()
     }
 
     override fun observerViewModel() {
-
+        with(viewModel) {
+            onQrCodeScannerEvent.observe(this@GetMainFragment, Observer {
+                startActivityForResult(Intent(requireActivity(), QrCodeActivity::class.java), REQUEST_CODE_QR_SCAN)
+            })
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE_QR_SCAN) {
+            if (data == null) {
+                Toast.makeText(requireContext(), "QR 코드 내용이 없습니다.", Toast.LENGTH_LONG).show()
+                return
+            }
+
+            val result = data.getStringExtra("com.blikoon.qrcodescanner.got_qr_scan_relult")
+            Toast.makeText(requireContext(), result, Toast.LENGTH_LONG).show()
+        }
+    }
 }
