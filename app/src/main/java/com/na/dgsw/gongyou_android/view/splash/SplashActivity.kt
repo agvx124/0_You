ceeab88bd48b44c981ab3c1f13b0127f80d2d409
Dong_Hyck_Na <43693001+agvx124@users.noreply.github.com>
2020-07-10 13:56:18 +0900

    [FIX] Splash Screen
    FIX

--- app/src/main/java/com/na/dgsw/gongyou_android/view/splash/SplashActivity.kt
+++ app/src/main/java/com/na/dgsw/gongyou_android/view/splash/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.na.dgsw.gongyou_android.view.splash
 
+import android.animation.Animator
+import android.content.Intent
+import android.util.Log
 import com.na.dgsw.gongyou_android.BR
 import com.na.dgsw.gongyou_android.R
 import com.na.dgsw.gongyou_android.base.BaseActivity
 import com.na.dgsw.gongyou_android.databinding.ActivitySplashBinding
+import com.na.dgsw.gongyou_android.view.main.MainActivity
 import com.na.dgsw.gongyou_android.viewmodel.SplashViewModel
 
 class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>() {
@@ -19,7 +23,30 @@ class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>() {
     }
 
     override fun setUp() {
+        binding.animationView.playAnimation()
 
+        binding.animationView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+                Log.e("Animation:","onAnimationRepeat");
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                Log.e("Animation:","onAnimationEnd");
+
+                Thread.sleep(1000)
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                Log.e("Animation:","onAnimationCancel");
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+                Log.e("Animation:","onAnimationStart");
+            }
+
+        })
     }
 
     override fun observerViewModel() {
